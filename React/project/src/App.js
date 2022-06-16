4b95c5b2f5f091726b595627c2b02387d1fba8e9
Ratchanon Traitiprat <teenoigammer2546@gmail.com>
2022-06-16 13:58:03 +0700

    JSX-1

--- React/project/src/App.js
+++ React/project/src/App.js
@@ -1,13 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
-import Hello from './components/Hellocomponent';
 
 function App() {
   return (
-    <div className="App">
-      <Hello />
-    </div>
+    <>
+      <section>
+        <article>
+          <h1>Hello React</h1>
+          <p>Record</p>
+          <ul>
+            <li> Traveller <span>200</span></li>
+            <li> Traveller <span>300</span></li>
+            <li> Traveller <span>400</span></li>
+          </ul>
+        </article>
+      </section>
+    </>
   );
 }
-
 export default App;
--- React/project/src/index.js
+++ React/project/src/index.js
@@ -1,22 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Hello from './components/Hellocomponent';
 
-//class components
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
-class Hellocomponentss extends React.Component {
-  render() {
-    return <h1>Hello  first components</h1>
-  }
-}
 
-//create components
-function Hellocomponents() {
-  return <h1>Ratchanon Traitiprat</h1>
-}
-ReactDOM.createRoot(<Hellocomponentss />, document.getElementById('root'));
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
